56f103be9905e500b32c2c6b6555a86d074cc45d
MyNgoc1 <145568770+MyNgoc1@users.noreply.github.com>
2024-03-18 20:54:21 +0700

    Update MainActivity.java

--- DanhSachTinhThanh/app/src/main/java/ntu/nt21/danhsachtinhthanh/MainActivity.java
+++ DanhSachTinhThanh/app/src/main/java/ntu/nt21/danhsachtinhthanh/MainActivity.java
@@ -1,15 +1,18 @@
 package ntu.nt21.danhsachtinhthanh;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> dsTenTinhThanhVN; //khai bao
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         //tu co so dl(sql,nosql, XML,...)
         //hard-code dl truc tiep
         //can bien phu hop chua dl
-        ArrayList<String> dsTenTinhThanhVN; //khai bao
+
         dsTenTinhThanhVN = new ArrayList<String>(); //xin moi
         //them dl (dung ra phai doc dl tu 1 nguon)
         //nhung ta hard-code(cho san de demo)
@@ -32,13 +35,27 @@ public class MainActivity extends AppCompatActivity {
         dsTenTinhThanhVN.add("Nha Trang");
         //b2. Tao adapter
         ArrayAdapter<string> adapterTinhThanh;
-        adapterTinhThanh =new ArrayAdapter<string>(context: this, android.R.layout.simple_list_item_1,dsTenTinhThanhVN)
+        adapterTinhThanh =new ArrayAdapter<string>(context:this , android.R.layout.simple_list_item_1,dsTenTinhThanhVN);
         //B3:Gan vao dieu khien hien thi listview
         ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
         lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        //gan bo lang nghe
+        lvTenTinhThanh.setOnItemClickListener(BoLangNghevaXL);
 
     }
+    //Tao bo lang nghe va xu ly su kien OnItemClick
+    //vd:BoLangNghevaXL
+    AdapterView.OnItemClickListener BoLangNghevaXL =new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+            //code xu ly o day
+            //i la vi tri phan tu vua duoc click
+            //vtri phan tu vua chon
+            String strTenTinhChon dsTenTinhThanhVN.get(i);
 
+            Toast.makeText(MainActivity.this, "ban vua chon:"+String.valueOf(i),Toast.LENGTH_LONG).show();
 
+        }
+    };
 
 }
