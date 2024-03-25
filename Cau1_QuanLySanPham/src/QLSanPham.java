7cd6fb272fe14ee41a1d8664bf6fc3294b99329c
MyNgoc1 <145568770+MyNgoc1@users.noreply.github.com>
2024-03-25 21:50:54 +0700

    Update QLSanPham.java

--- Cau1_QuanLySanPham/src/QLSanPham.java
+++ Cau1_QuanLySanPham/src/QLSanPham.java
@@ -97,6 +97,12 @@ public class QLSanPham extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	public QLSanPham() {
+		Tieude();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 423);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 	
 }
